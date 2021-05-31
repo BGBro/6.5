e0659cdaaf3c62350c8186fd82999ed1aab70a92
BGBro <bohdan.horbachov.itis.2020@lpnu.ua>
2021-05-31 13:57:23 +0300

    master

---
+++ 6.5/6.5.cpp
@@ -0,0 +1,57 @@
+﻿
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template <class T>
+class Stack {
+public:
+    typedef T value_type;
+    vector <value_type> a;
+    Stack(const T& t = T());
+
+    bool isEmpty() 
+    {
+        if (a.size() == 0) {
+            return true;
+        }
+        return false;
+    }
+    value_type Top() throw out_of_range()
+    {
+        if (a.size() != 0)
+        {
+            return a.back();
+        }
+        else
+        {
+            throw out_of_range("Index out of range!");
+        }
+    }
+    void Push(value_type element) 
+    {
+        a.push_back(element);
+    }
+    void Pop() 
+    {
+        a.pop_back();
+    }
+};
+
+template <class T>
+Stack<T>::Stack(const T& t)
+{
+    a.push_back(t);
+}
+
+int main() 
+{
+    Stack<string> a("programming");
+    a.Pop();
+    cout << a.Top() << endl;
+    a.Push("top");
+    cout << a.Top() << endl;
+    a.Pop();
+    cout << a.Top() << endl;
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../6.5/6.5.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Stack<int> a(5);
+
+			a.Push(3);
+
+			Assert::AreEqual(3, a.Top());
+
+		}
+	};
+}
